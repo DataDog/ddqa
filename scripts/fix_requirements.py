2af9c9b2f29006dd586549b14885bd49782cec49
Ofek Lev <ofekmeister@gmail.com>
2023-04-09 23:10:51 -0400

    Cleanup

--- scripts/fix_requirements.py
+++ scripts/fix_requirements.py
@@ -2,18 +2,8 @@ from pathlib import Path
 
 
 def main():
-    # In order to cross-compile all dependencies must have wheels, see:
-    # https://pyoxidizer.readthedocs.io/en/stable/pyoxidizer_packaging_python_files.html#choosing-which-packaging-method-to-call
-    # https://github.com/Textualize/textual/pull/2025
-    requirements_file = Path('requirements.txt')
-    requirements = ['ddqa']
-
-    for line in requirements_file.read_text(encoding='utf-8').splitlines():
-        package = line.split('==')[0]
-        if package != 'mkdocs-exclude==':
-            requirements.append(package)
-
-    requirements_file.write_text('\n'.join(requirements), encoding='utf-8')
+    with Path('requirements.txt').open('a', encoding='utf-8') as f:
+        f.write('\nddqa\n')
 
 
 if __name__ == '__main__':
