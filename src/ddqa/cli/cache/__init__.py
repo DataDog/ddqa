7fee8d3415415ad12785f24b15f90dfbd26db86d
Florent Clarret <florent.clarret@datadoghq.com>
2023-11-14 17:56:54 +0100

    Add commands to interact with the cache

---
+++ src/ddqa/cli/cache/__init__.py
@@ -0,0 +1,18 @@
+# SPDX-FileCopyrightText: 2023-present Datadog, Inc. <dev@datadoghq.com>
+#
+# SPDX-License-Identifier: MIT
+import click
+
+from ddqa.cli.cache.explore import explore
+from ddqa.cli.cache.find import find
+from ddqa.cli.cache.remove import remove
+
+
+@click.group(short_help='Manage the cache')
+def cache():
+    pass
+
+
+cache.add_command(explore)
+cache.add_command(find)
+cache.add_command(remove)
